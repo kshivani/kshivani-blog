fed7103fa8d6abd42948df25ca68600c39d88280
kshivani <kshivanisingh@yahoo.in>
2016-02-21 01:32:27 +0530

    first_django_App

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .forms import PostForm
+from .models import Post
+def post_list(request):
+	posts = Post.objects.all()	
+	return render(request, 'blog/post_list.html', {'posts': posts})
 
-# Create your views here.
+
+def post_new(request):
+	form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
